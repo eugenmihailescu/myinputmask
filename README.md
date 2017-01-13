1b8dc810d5fbad9d50534c5c85a8a9075b13850e
eugenmihailescu <eugenmihailescux@gmail.com>
2017-01-13 20:48:17 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# myinputmask
+# my-input-mask
 Input mask plugin for JavaScript
+
+A lightweight plugin (only 2K minimized) that adds input mask functionality to the HTML inputs elements.
+
+## Features
+- formats the input value as defined by input's mask (see `mask`)
+- accepts only the char class given by pattern (see `pattern`)
+- limits the length of input value by using the mask length (see `strict` attribute)
+- without
+ 
+## Requirements
++ JavaScript capable browser (eg. IE9+, FF, Chrome, Opera, etc)
+
+## How it works
+The plugin class is a JavaScript function called `InputMask` which when executed it binds some key events (eg. keydown, keyup) to the configured/given input elements and that's it!
+
+To tell the function what are your inputs and how to mask them it accepts a configuration object as first argument with the following definition:
+```javascript
+object = {
+  inputs: {input_def1, input_def2,..},
+  mask_symbol: "_",
+  autoinit: boolean
+}
+```
+where
+ - `inputs` is a list of `input_def` which have the following definition:
+   - an `input_def` element is just an object with three properties:
+     - `mask` : a string that denotes the input mask (when not specified the input's [placeholder attribute](https://developer.mozilla.org/en/docs/Web/HTML/Element/input#attr-placeholder) is used if exists)
+     - `pattern` : a string that represents the character class accepted for inputs, eg. *`[0-9A-Z]`* or *`[^@&!]`* (besides the home|end|left|right|delete|backspace|caps-lock|tab|shift-tab|ctrl-a|ctrl-v keys which are allowed by default)
+     - `strict` : a boolean which tells the function if the mask length should limit the input value length (`true`) or not (`false`). When not specified it is `true` by default.
+- `mask_symbol`, if specified, is the mask char used within each input_def element (default to underscore "`_`")
+- `autoinit` tells the class to auto-initialize automatically or manually, by calling its `init()` function.
+
+*Example of an `input_def`*:
+```javascript
+var input_def = {
+  "#a-phone-number" : {
+    mask: "(___) ___-____", // where the underscore is the mask symbol
+    pattern: "[0-9A-Z]", // only digits and caps letters
+    strict: true // limits the value length to the mask length
+  }
+} 
+```
+Of course, you may have a dosen of inputs so you can just wrap them all into an object and pass that object (inline or as a variable) to the function constructor.
+Note that by using a class selector (or alike) you may select multiple elements at once, and therefore apply the formating to all of them with only one input_def definition.
+## Usage
+A quick example of two input elements (a card number and *something* realy odd) and their mask definition:
+```html
+<!-- some plain html -->
+<input id="my-selector" type="text">
+<input name="whatever" type="tel" class="my-class">
+```   
+```javascript
+// create an instance of our class that takes care of the rest
+new InputMask({
+  inputs: {
+    '#my-selector': {
+      mask: '____ ____ ____ ____',
+      strict: true,
+      pattern: '[0-9]'
+    },
+    'input[type=tel]': {
+      mask: '___%___%___',
+      strict: true,
+      pattern: '[^@!&]'
+    }
+  },
+  mask_symbol: '_' // when underscore is used we can actually omit this parameter
+});
+```
+will mask the input editor with Id `#my-selector` and any input with `type=tel`.
+
+## Caveat Emptor
+1. When you'll read the input value you will find that its value includes also those mask separators that were introduced by the mask (not only the text that was physically entered by the end-user). That is something expected because the input value was physically altered by the plug-in. To overcome this you may read the input value using a special function (see `getUnmaskedValue`) provided by class
+2. The insert key will not work as in a regular editor (this may, however, be fixed in a future release).
+
+### How to retrieve the unmasked value
+Since the input value is masked by using a given mask/format you cannot read directly the DOM element's value and retrieve its unmasked value. However, there is a special function to deal with these situations: `getUnmaskedValue(input)` where `input` is the DOM input element for which you want to retrieve the unmasked value.
+
+**Feedback** : drop me a line at eugenmihailescux at gmail dot com
---
+++ src/InputMask.js
@@ -0,0 +1,299 @@
+/**
+ * Adds input mask functionality to your HTML inputs elements.
+ * 
+ * @param config
+ *            An object with two properties: an list of input elements and optionally the mask symbol.
+ * @returns Returns the object instance
+ */
+/**
+ * @param config
+ * @returns
+ */
+function InputMask(config) {
+    "use strict";
+    var _this_ = {};
+    _this_.name = "InputMaskPlugin";
+
+    _this_.inputs = config.inputs || {};
+    _this_.mask_symbol = config.mask_symbol || "_";
+    _this_.autoinit = config.autoinit || true;
+    _this_.wait = false;
+    var UNDEF = "undefined";
+    var CHAR = "character";
+
+    var KEY_BKSP = "Backspace";
+    var KEY_DEL = "Delete";
+    var KEY_TAB = "Tab";
+    var NAV_KEYS = [ "Shift", "Control", "Home", "End", "ArrowLeft", "ArrowRight", "CapsLock", KEY_DEL, KEY_BKSP, KEY_TAB ];
+
+    /**
+     * Get the caret position within the given input element
+     * 
+     * @param element
+     *            The selection element
+     * @return The caret position within the given input element
+     */
+    function getCaretPosition(element) {
+        if (UNDEF === typeof element) {
+            return false;
+        }
+
+        var pos = 0;
+        if (element.selectionStart) {
+            pos = element.selectionStart;
+        } else if (document.selection) {
+            element.focus();
+            var Sel = document.selection.createRange();
+            var SelLength = document.selection.createRange().text.length;
+            Sel.moveStart(CHAR, -element.value.length);
+            pos = Sel.text.length - SelLength;
+        }
+        return pos;
+    }
+
+    /**
+     * Select a given range within the current `_this_` element
+     * 
+     * @param start
+     *            The starting index for selection
+     * @param end
+     *            The last index of selection
+     */
+    function selectRange(start, end) {
+        if (end === undefined) {
+            end = start;
+        }
+        if (_this_.selectionStart) {
+            _this_.selectionStart = start;
+            _this_.selectionEnd = end;
+        } else if (_this_.setSelectionRange) {
+            _this_.setSelectionRange(start, end);
+        } else if (_this_.createTextRange) {
+            var range = _this_.createTextRange();
+            range.collapse(true);
+            range.moveEnd(CHAR, end);
+            range.moveStart(CHAR, start);
+            range.select();
+        }
+    }
+
+    /**
+     * Callback for the keydown keyboard event
+     * 
+     * @param event
+     *            KeyboardEvent
+     */
+    function onKeyDown(event) {
+
+        if (_this_.wait) {
+            event.preventDefault();
+            return;
+        }
+
+        var elem = event.currentTarget;
+        var data = elem.dataset;
+        var key = event.key;
+        var caret_pos = getCaretPosition(elem);
+
+        // not in control & navigation keys
+        if (!(event.ctrlKey || event.shiftKey) && NAV_KEYS.indexOf(key) < 0) {
+            if (!new RegExp(data['pattern']).test(key)) {
+                event.preventDefault();
+            }
+
+            // strict mode => don"t exceed the mask length
+            if (data['strict'] === 'true' && elem.value.length >= data['mask'].length) {
+                if (window.getSelection().rangeCount <= 1) {
+                    event.preventDefault();
+                }
+            }
+        }
+        // backspace near a mask separator
+        if (false !== caret_pos) {
+            if (KEY_BKSP === key && data['mask'][caret_pos - 1] !== _this_.mask_symbol) {
+                selectRange.call(elem, caret_pos - 1, caret_pos - 1);
+            } else {
+                // delete near a mask separator
+                if (KEY_DEL === key && data['mask'][caret_pos] !== _this_.mask_symbol) {
+                    selectRange.call(elem, caret_pos + 1, caret_pos + 1);
+                }
+            }
+        }
+
+        _this_.wait = NAV_KEYS.indexOf(key) < 0;
+    }
+
+    /**
+     * Callback for the keyup keyboard event
+     * 
+     * @param event
+     *            KeyboardEvent
+     */
+    function onKeyUp(event) {
+        var elem = event.currentTarget;
+        var data = elem.dataset;
+        var caret_pos = getCaretPosition(elem);
+        var key = event.key;
+        var string = elem.value;
+        var ctrlFnKeys = [ "a", "A", "v", "V" ];
+
+        // allow Ctrl-A and Ctrl-V
+        if (event.ctrlKey) {
+            if (ctrlFnKeys.indexOf(key) >= 0) {
+                _this_.wait = false;
+                return;
+            }
+        } else if (event.shiftKey) {
+            // allow shift-TAB
+            if (key === KEY_TAB) {
+                _this_.wait = false;
+                return;
+            }
+        } else {
+            if (NAV_KEYS.indexOf(key) < 0 && false !== caret_pos) {
+                if (caret_pos < string.length) {
+
+                    var unmasked = _this_.getUnmaskedValue.call(elem);
+                    var masked = _this_.maskString.call(elem, unmasked);
+
+                    elem.value = masked;
+                    caret_pos = masked.length;
+                }
+
+                if (caret_pos < data['mask'].length && data['mask'][caret_pos] !== _this_.mask_symbol) {
+                    elem.value = string + data['mask'][caret_pos];
+                    selectRange.call(elem, caret_pos + 1, caret_pos + 1);
+                }
+            }
+        }
+
+        _this_.wait = false;
+    }
+
+    /**
+     * Mask the given string by using the current `_this_` element mask definition
+     * 
+     * @param string
+     *            The string to mask
+     * @return Returns the masked string
+     */
+    _this_.maskString = function(string) {
+        var i;
+        var elem = this;
+        var data = elem.dataset;
+        var strict;
+
+        strict = UNDEF === typeof strict ? true : strict;
+
+        var mask_count = 0;
+        var offset = 0;
+        var head = 0;
+
+        var output = "";
+
+        // the final string may include the mask separators
+        for (i = 0; !strict && i < data['mask'].length; i += 1) {
+            offset += data['mask'][i] !== _this_.mask_symbol ? 1 : 0;
+        }
+
+        for (i = 0; i < Math.max(string.length, data['mask'].length) + offset; i += 1) {
+            if ((strict && i >= data['mask'].length) || (i >= string.length + mask_count)) {
+                break;
+            }
+            if (i < data['mask'].length && data['mask'][i] !== _this_.mask_symbol) {
+                output += data['mask'][i];
+                mask_count += 1;
+            } else {
+                if (head < string.length) {
+                    output += string[head];
+                } else {
+                    output += data['mask'][i];
+                    mask_count += 1;
+                }
+                head += 1;
+            }
+        }
+
+        return output;
+    };
+
+    /**
+     * Removes those chars that represents the mask
+     */
+    _this_.getUnmaskedValue = function() {
+        var elem = this;
+        var string = elem.value;
+        var data = elem.dataset;
+        var strict = UNDEF === typeof data['strict'] ? true : data['strict'];
+        var i;
+
+        var output = "";
+        for (i = 0; i < Math.max(string.length, data['mask'].length); i += 1) {
+            if (i >= string.length) {
+                break;
+            }
+            if (i < data['mask'].length) {
+                if (new RegExp(data['pattern']).test(string[i])) {
+                    output += string[i];
+                }/*
+                     * else if (data.mask[i] !== _this_.mask_symbol) { continue; }
+                     */
+            } else {
+                break;
+            }
+        }
+
+        // if string longer than mask, get the rest fragment of string
+        if (strict !== 'true') {
+            output += string.slice(data['mask'].length);
+        }
+        return output;
+    };
+
+    /**
+     * Initialize the inputs for handling the mask events
+     */
+    _this_.init = function() {
+        // process all the inputs
+        Object.keys(_this_.inputs).forEach(function(selector) {
+            var prop = _this_.inputs[selector];
+            var selected_elements = document.querySelectorAll(selector);
+
+            // add the keydown|keyup listeners to each selected item
+            Object.keys(selected_elements).forEach(function(index) {
+
+                var elem = selected_elements[index];
+
+                // it seems there is another running instance that already attached to _this_ element
+                if (elem.getAttribute("plugin") === _this_.name) {
+                    return;
+                }
+
+                var elem_data = {
+                    'mask' : prop['mask'] || elem.getAttribute("placeholder"),
+                    'pattern' : prop['pattern'] || elem.getAttribute("pattern"),
+                    'strict' : prop['strict'].toString(),
+                    'plugin' : _this_.name
+                };
+                // store the mask within element dataset
+                Object.keys(elem_data).forEach(function(p) {
+                    elem.dataset[p] = elem_data[p];
+                });
+
+                // prevent key actions
+                elem.addEventListener("keydown", onKeyDown);
+
+                // process key inputs
+                elem.addEventListener("keyup", onKeyUp);
+
+            });
+        });
+    };
+
+    if (_this_.autoinit) {
+        _this_.init();
+    }
+
+    return _this_;
+}
+window['InputMask'] = InputMask;
